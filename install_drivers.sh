4c086cc9c1d4c06e8d5274635a5a57d99fa0bb5b
Beny Hdez <beny_hm@hotmail.com>
2024-02-27 15:24:34 -0800

    Nvidia Drivers Installation Experimental

--- install_drivers.sh
+++ install_drivers.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
 installNvidiaDrivers() {
+    switchToTestingSource
     # Installing the appropriate GPU drivers
     sudo apt-get install nvidia-driver nvidia-opencl-icd libcuda1 libglu1-mesa
     # For h.264 and h.265 export you also need the NVIDIA encode library:
     sudo apt-get install libnvidia-encode1
+    rollBackSource
 }
 
 installAMDPRO() {
@@ -26,7 +28,7 @@ installMesaDrivers() {
     sudo apt update
     sudo apt install -y mesa-vulkan-drivers
 
-    echo -e "\n#################### Installing firmwares, tools and Steam ####################\n"
+    print_log "\n#################### Installing firmwares, tools and Steam ####################\n"
     sudo apt install -y neofetch mangohud git mesa-opencl-icd steam-installer bash-completion vulkan-tools firmware-linux firmware-linux-free firmware-linux-nonfree firmware-amd-graphics
     sudo apt clean
     rollBackSource
@@ -51,6 +53,7 @@ rollBackSource() {
     sudo rm /etc/apt/sources.list
     sudo wget https://github.com/BenyHdezM/Debian4Gamers/raw/main/stable_sources.list -O /etc/apt/sources.list
     sudo apt update
+    sudo apt autoremove -y
 }
 
 installDrivers() {
