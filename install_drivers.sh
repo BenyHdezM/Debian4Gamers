6e0258c967fe5c4f5697faf71643f95f99f4ace1
Beny Hdez <beny@GamingPc.home>
2024-02-28 09:34:27 -0800

    steam all libs

--- install_drivers.sh
+++ install_drivers.sh
@@ -30,6 +30,7 @@ installMesaDrivers() {
     # sudo apt install mesa-vulkan-drivers steam-libs steam-libs-i386 steam-devices
     wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb
     sudo apt install ./steam.deb
+    sudo apt install libc6:amd64 libc6:i386 libegl1:amd64 libegl1:i386 libgbm1:amd64 libgbm1:i386 libgl1-mesa-dri:amd64 libgl1-mesa-dri:i386 libgl1:amd64 libgl1:i386 steam-libs-amd64:amd64
     rm steam.deb
     rollBackSource
 }
