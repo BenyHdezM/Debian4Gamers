7eb8223c35efcc8920a9544d4654e6d10d7b6fa6
Beny Hdez <beny@GamingPc.home>
2024-02-28 10:46:26 -0800

    rename gpu instant replay

--- gnome_instant_replay/install.sh
+++ gnome_instant_replay/install.sh
@@ -13,7 +13,7 @@ custom_keybindings_array="${custom_keybindings_array}, '${new_binding}'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[${custom_keybindings_array}]"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ name 'Instant Replay on/off'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ command '/home/beny/.config/gpu-screen-recorder/start-replay.sh'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ binding '<Alt>F9'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ binding '<Shift><Alt>F9'
 
 
 existing_keybindings=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
@@ -27,4 +27,4 @@ custom_keybindings_array="${custom_keybindings_array}, '${new_binding}'"
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[${custom_keybindings_array}]"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ name 'Instant Replay Save Replay'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ command '/home/beny/.config/gpu-screen-recorder/save-replay.sh'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ binding '<Alt>F10'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom$next_index/ binding '<Shift><Alt>F10'
