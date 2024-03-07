39a29a3735ef332f22b84a304f3e45225d126434
Beny Hdez <beny_hm@hotmail.com>
2024-03-07 12:54:15 -0800

    Adding a better solution for Instant Recording on X11

--- gnome_instant_replay/start-replay.sh
+++ gnome_instant_replay/start-replay.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+\#!/bin/sh
 
 video_path="$HOME/Videos"
-seconds=60
+seconds=180
 
 if pgrep -x "gpu-screen-reco" >/dev/null; then
-  killall -SIGINT gpu-screen-recorder
-  notify-send "GPU Instant Replay" "Instant Replay has stopped"
+	killall -SIGINT gpu-screen-recorder
+	# notify-send -u normal -t 1000 "GPU Instant Replay" "Instant Replay $window_name has stopped"
+	zenity --window-icon="$window_icon" --warning --text="Instant Replay is no longer active." --timeout=1 --no-markup
 else
-  notify-send "GPU Instant Replay" "Instant Replay has started"
-  /usr/bin/flatpak run --command=gpu-screen-recorder com.dec05eba.gpu_screen_recorder -w screen -f $seconds -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -r 30 -c mp4 -o "$video_path"
+	window=$(xdotool selectwindow)
+	echo $window
+	if [ -n "$window" ]; then
+		window_name=$(xdotool getwindowclassname "$window" || xdotool getwindowname "$window" || echo "game")
+		window_icon=$(xprop -id $window | awk -F '"' '/WM_ICON/{print $2}' | tail -n 1)
+		# notify-send -u normal -t 1000 "GPU Instant Replay" "Instant Replay $window_name has started" -i "$window_icon"
+		zenity --window-icon="$window_icon" --warning --text="Instant Replay $window_name has begun" --timeout=1 --no-markup
+		window_id=$(xdotool search --onlyvisible --class "zenity" | tail -1)
+
+		# Move the window to the top of the screen
+		xdotool windowmove $window_id 0 0
+		/usr/bin/flatpak run --command=gpu-screen-recorder com.dec05eba.gpu_screen_recorder -w "$window" -f $seconds -a "$(pactl get-default-sink).monitor|$(pactl get-default-source)" -c mp4 -r 60 -o "$HOME/Videos/$window_name"
+	fi
 fi
